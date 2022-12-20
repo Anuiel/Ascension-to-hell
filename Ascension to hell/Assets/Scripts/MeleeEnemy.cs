67e72174c255a96159e0dfc33ca64e4a6bd14966
Anuiel <n00t_noot@yandex.ru>
2022-12-20 13:55:36 +0300

    Quit implemented

--- Ascension to hell/Assets/Scripts/MeleeEnemy.cs
+++ Ascension to hell/Assets/Scripts/MeleeEnemy.cs
@@ -8,30 +8,12 @@ public class MeleeEnemy : MonoBehaviour
     GameObject player;
     [SerializeField]
     float speed;
-    [SerializeField]
-    float attack_range;
 
     [SerializeField]
     Rigidbody2D rb;
 
     private float distanse;
 
-    void Awake()
-    {
-        // rb = gameObject.AddComponent<Rigidbody2D>();
-    }
-    // Update is called once per frame
-    // void Update()
-    // {  
-    //     distanse = Vector2.Distance(transform.position, player.transform.position);
-    //     Vector2 direction = player.transform.position - transform.position;
-    //     direction = direction.normalized;
-
-    //     float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
-
-    //     transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-    //     transform.rotation = Quaternion.Euler(Vector3.forward * angle);
-    // }
     void FixedUpdate()
     {
         Vector2 direction = player.transform.position - transform.position;
