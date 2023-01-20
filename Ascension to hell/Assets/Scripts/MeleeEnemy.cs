6c8ae214aa0096a68605a6847b10c36ca0347b62
MonsterOnLeash <660867@mail.ru>
2023-01-21 02:52:54 +0300

    a star and minor fixes

--- Ascension to hell/Assets/Scripts/MeleeEnemy.cs
+++ Ascension to hell/Assets/Scripts/MeleeEnemy.cs
@@ -1,26 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class MeleeEnemy : MonoBehaviour
-{   
-    [SerializeField]
-    GameObject player;
-    [SerializeField]
-    float speed;
+{
+    //    [SerializeField]
+    //   GameObject player;
+    //    [SerializeField]
+    //    float speed;
+    //
+    //    [SerializeField]
+    //    Rigidbody2D rb;
+    //
+    //    private float distanse;
 
-    [SerializeField]
-    Rigidbody2D rb;
+    //    void FixedUpdate()
+    //    {
+    //        Vector2 direction = player.transform.position - transform.position;
+    //        direction = direction.normalized;
+    //
+    //        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+    //        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+    //       rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
+    //    }
 
-    private float distanse;
-
-    void FixedUpdate()
+    private void Awake()
     {
-        Vector2 direction = player.transform.position - transform.position;
-        direction = direction.normalized;
-
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
-        rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
+        GetComponent<AIDestinationSetter>().target = GameObject.Find("Player").GetComponent<Transform>();
     }
 }
